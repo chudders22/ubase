e90c92c8d8a377c87a35d76d7d7a825cae4d137d
chudders22 <anthony.chudley@gmail.com>
2020-10-19 11:38:22 +0100

    Updates

---
+++ src/Application.Web/App_Plugins/BlockListViews/js/image.controller.js
@@ -0,0 +1,19 @@
+﻿angular.module("umbraco").controller("ContentBlocks.Previews.ImageController", function ($scope, entityResource) {
+    var vm = this;
+    vm.imageUrl = null;
+    function loadImage(propertyValue) {
+        if (propertyValue != "") {
+            entityResource.getById(propertyValue, "Media").then(function (ent) {
+                vm.imageUrl = ent.metaData.MediaPath;
+            });
+        } else {
+            vm.imageUrl = null;
+        }
+    }
+    loadImage($scope.block.data.image);
+    $scope.$watch("block.data.image", function (newValue, oldValue) {
+        if (newValue === oldValue) return;
+        loadImage(newValue);
+    });
+
+});
---
+++ src/Application.Web/App_Plugins/BlockListViews/js/multipleimages.controller.js
@@ -0,0 +1,18 @@
+﻿angular.module("umbraco").controller("ContentBlocks.Previews.MultipleImageController", function ($scope, entityResource) {
+    var vm = this;
+    vm.imageUrls = [];
+    const getUrl = async (url) => {
+        var media = await entityResource.getById(url, "Media")
+        return media
+    }
+    const getImages = async (images) => {
+        var imageIds = images.split(",");
+        return Promise.all(imageIds.map(x => getUrl(x)))
+    }
+    getImages($scope.block.data.images).then(x =>  vm.imageUrls = x )
+    $scope.$watch("block.data.images", function (newValue, oldValue) {
+        if (newValue === oldValue) return;
+        getImages(newValue).then(x => vm.imageUrls = x);
+    });
+
+});
